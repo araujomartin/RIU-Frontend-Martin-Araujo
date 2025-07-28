4cd792642ceed5c2b644229cc8120a2426c35910
Martin Araujo <martinaraujo484@gmail.com>
2025-07-28 16:18:00 -0300

    chore: add ESLint config and rules

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      // JS rules
+      "no-console": "off",
+      "indent": [
+        "error",
+        4
+      ],
+      "arrow-parens": "warn",
+      "semi": "warn",
+      "no-multiple-empty-lines": [
+        "error",
+        {
+          "max": 1,
+        }
+      ],
+      "quotes": [
+        "error",
+        "single",
+        { "avoidEscape": true }
+      ],
+      // Angular rules
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
