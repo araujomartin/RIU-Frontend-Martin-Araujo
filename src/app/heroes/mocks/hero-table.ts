b6d19022cef825600a2d0683d85026169bf88079
Martin Araujo <martinaraujo484@gmail.com>
2025-07-31 09:45:02 -0300

    test: add error input in hero-table component

--- src/app/heroes/mocks/hero-table.ts
+++ src/app/heroes/mocks/hero-table.ts
@@ -10,5 +10,7 @@ export class MockHeroTable {
     public readonly paginatedHeroes = model<Pagination<Hero>>();
     public readonly isLoading = input(false);
 
+    public readonly error = input<Error>();
+
     public readonly perPage = input(10);
 }
